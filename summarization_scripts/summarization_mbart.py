377e857038857755cbe48ec2b35175c05dd2a965
vladsolovyev <vlsol90@yahoo.com>
2023-01-20 11:20:28 +0100

    Train and tune different models. Including zero and few shot models based on english

--- summarization_scripts/summarization_mbart.py
+++ summarization_scripts/summarization_mbart.py
@@ -1,16 +1,11 @@
 import evaluate
 import nltk
 import numpy as np
-from datasets import load_dataset
 
 from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer, \
     MBartTokenizer, MBartForConditionalGeneration
 
 nltk.download('punkt')
-
-data_es = load_dataset("GEM/xlsum", "spanish")
-data_en = load_dataset("GEM/xlsum", "english")
-data_ru = load_dataset("GEM/xlsum", "russian")
 
 
 def calculate_rouge_score(predictions, references):
@@ -28,33 +23,45 @@ def calculate_bert_score(predictions, references):
     return {"bert_score_{}".format(k): np.mean(v) for k, v in bert_result.items()}
 
 
-class MBartModel:
+def create_training_arguments(output_dir):
+    batch_size = 4
+    return Seq2SeqTrainingArguments(
+        output_dir,
+        evaluation_strategy="epoch",
+        save_strategy="no",
+        logging_steps=100,
+        do_train=True,
+        do_eval=True,
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        weight_decay=0.01,
+        num_train_epochs=1,
+        predict_with_generate=True,
+        fp16=False,
+        overwrite_output_dir=True
+    )
+
+
+class MBartSummarizationModel:
     def __init__(self,
-                 dataset,
                  model_name="facebook/mbart-large-cc25",
                  max_input_length=1024,
                  max_target_length=512,
                  src_lang="en_XX",
-                 tgt_lang="en_XX"):
+                 tgt_lang="en_XX",
+                 output_dir=None):
         self.model_name = model_name
         self.max_input_length = max_input_length
         self.max_target_length = max_target_length
-        self.tokenizer = MBartTokenizer.from_pretrained(model_name, model_max_length=max_input_length,
+        self.tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-cc25", model_max_length=max_input_length,
                                                         src_lang=src_lang, tgt_lang=tgt_lang)
+        self.summarization_model = MBartForConditionalGeneration.from_pretrained(self.model_name)
+        self.summarization_model.config.decoder_start_token_id = self.tokenizer.lang_code_to_id[tgt_lang]
+        self.summarization_model.config.forced_bos_token_id = self.tokenizer.lang_code_to_id[tgt_lang]
+        self.summarization_model.tokenizer = self.tokenizer
+        self.summarization_model.output_dir = output_dir
         self.summarization_trainer = None
-        self.tokenized_datasets = dataset.map(self.preprocess_function, batched=True)
-
-    def preprocess_function(self, data):
-        model_inputs = self.tokenizer(data["text"],
-                                      max_length=self.max_input_length,
-                                      truncation=True)
-
-        summaries = self.tokenizer(text_target=data["target"],
-                                   max_length=self.max_target_length,
-                                   truncation=True)
-
-        model_inputs["labels"] = summaries["input_ids"]
-        return model_inputs
 
     def decode_labels(self, predictions, labels):
         decoded_preds = self.tokenizer.batch_decode(predictions, skip_special_tokens=True)
@@ -79,59 +86,27 @@ class MBartModel:
 
         return {k: round(v, 4) for k, v in result.items()}
 
-    def create_training_arguments(self):
-        batch_size = 4
-        model_name = self.model_name.split("/")[-1]
-        return Seq2SeqTrainingArguments(
-            "{}-finetuned-xlsum".format(model_name),
-            evaluation_strategy="steps",
-            save_steps=15000,
-            eval_steps=15000,
-            logging_steps=100,
-            do_train=True,
-            do_eval=True,
-            learning_rate=2e-5,
-            per_device_train_batch_size=batch_size,
-            per_device_eval_batch_size=batch_size,
-            weight_decay=0.01,
-            save_total_limit=3,
-            num_train_epochs=1,
-            predict_with_generate=True,
-            fp16=False,
-            overwrite_output_dir=True,
-            load_best_model_at_end=True
-        )
-
-    def train(self):
-        summarization_model = MBartForConditionalGeneration.from_pretrained(self.model_name)
-        summarization_model.config.decoder_start_token_id = \
-            self.tokenizer.lang_code_to_id[self.tokenizer.tgt_lang]
-        summarization_model.config.forced_bos_token_id = \
-            self.tokenizer.lang_code_to_id[self.tokenizer.tgt_lang]
-        data_collator = DataCollatorForSeq2Seq(self.tokenizer, model=summarization_model)
+    def train(self, train_data, validation_data, output_dir, save_model=False):
+        data_collator = DataCollatorForSeq2Seq(self.tokenizer, model=self.summarization_model)
         self.summarization_trainer = Seq2SeqTrainer(
-            summarization_model,
-            self.create_training_arguments(),
-            train_dataset=self.tokenized_datasets["train"],
-            eval_dataset=self.tokenized_datasets["validation"],
+            self.summarization_model,
+            create_training_arguments(output_dir),
+            train_dataset=train_data,
+            eval_dataset=validation_data,
             data_collator=data_collator,
-            tokenizer=self.tokenizer,
             compute_metrics=self.calculate_metrics
         )
         self.summarization_trainer.train(resume_from_checkpoint=False)
+        if save_model:
+            self.summarization_trainer.save_model(output_dir)
 
-    def test_predictions(self):
+    def test_predictions(self, test_data):
         test_results = self.summarization_trainer.predict(
-            self.tokenized_datasets["test"],
+            test_data,
             metric_key_prefix="test",
             max_length=self.max_target_length,
             num_beams=5
         )
         self.summarization_trainer.log_metrics("test", test_results.metrics)
         self.summarization_trainer.save_metrics("test", test_results.metrics)
-
-
-if __name__ == '__main__':
-    model = MBartModel(data_en)
-    model.train()
-    model.test_predictions()
+        return test_results.metrics
---
+++ summarization_scripts/training_runner.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+
+from datasets import load_dataset, Dataset
+from transformers import MBartTokenizer
+
+from summarization_scripts.summarization_mbart import MBartSummarizationModel
+
+MAX_INPUT_LENGTH = 1024
+MAX_TARGET_LENGTH = 512
+tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-cc25", model_max_length=MAX_INPUT_LENGTH)
+dataset_languages = ["english", "spanish", "russian"]
+model_languages = ["en_XX", "es_XX", "ru_RU"]
+tokenized_datasets = []
+
+
+def preprocess_function(dataset_split):
+    tokenizer.src_lang = model_language
+    tokenizer.tgt_lang = model_language
+    model_inputs = tokenizer(dataset_split["text"],
+                             max_length=MAX_INPUT_LENGTH,
+                             truncation=True)
+
+    summaries = tokenizer(text_target=dataset_split["target"],
+                          max_length=MAX_TARGET_LENGTH,
+                          truncation=True)
+
+    model_inputs["labels"] = summaries["input_ids"]
+    return model_inputs
+
+
+for dataset_language, model_language in zip(dataset_languages, model_languages):
+    dataset = load_dataset("GEM/xlsum", dataset_language)
+    tokenized_datasets.append(dataset.map(preprocess_function, batched=True))
+
+output_dir = datetime.today().strftime('%Y-%m-%d')
+
+# every language separately
+for tokenized_dataset, model_language in zip(tokenized_datasets, model_languages):
+    model = MBartSummarizationModel(src_lang=model_language, tgt_lang=model_language)
+    model.train(tokenized_dataset["train"],
+                tokenized_dataset["validation"],
+                "{}/{}".format(output_dir, model_language),
+                save_model=(model_language == "en_XX"))
+    model.test_predictions(tokenized_dataset["test"])
+
+# zero shot. Evaluate spanish and russian datasets using english model
+for tokenized_dataset, model_language in zip(tokenized_datasets[1:3], model_languages[1:3]):
+    model = MBartSummarizationModel(model_name="{}/en_XX".format(output_dir),
+                                    src_lang=model_language,
+                                    tgt_lang=model_language,
+                                    output_dir="{}/en_XX_zero_{}".format(output_dir, model_language))
+    model.test_predictions(tokenized_dataset["test"])
+
+# few shot experiments. Tune english model using few data from spanish and russian datasets
+for tokenized_dataset, model_language in zip(tokenized_datasets[1:3], model_languages[1:3]):
+    for data_size in [10, 100, 1000, 10000]:
+        model = MBartSummarizationModel(model_name="{}/en_XX".format(output_dir),
+                                        src_lang=model_language,
+                                        tgt_lang=model_language)
+        model.train(Dataset.from_dict(tokenized_dataset["train"][:data_size]),
+                    Dataset.from_dict(tokenized_dataset["validation"][:data_size]),
+                    "{}/en_XX_tuned_{}_{}".format(output_dir, model_language, data_size))
+        model.test_predictions(Dataset.from_dict(tokenized_dataset["test"][:data_size]))
+
+# tune english model using complete data from spanish and russian datasets
+for tokenized_dataset, model_language in zip(tokenized_datasets[1:3], model_languages[1:3]):
+    model = MBartSummarizationModel(model_name="{}/en_XX".format(output_dir),
+                                    src_lang=model_language,
+                                    tgt_lang=model_language)
+    model.train(tokenized_dataset["train"],
+                tokenized_dataset["validation"],
+                "{}/en_XX_tuned_{}".format(output_dir, model_language),
+                save_model=(model_language == "es_XX"))
+    model.test_predictions(tokenized_dataset["test"])
+
+# all three languages together
+model = MBartSummarizationModel(model_name="{}/en_XX_tuned_es_XX".format(output_dir),
+                                src_lang="ru_RU",
+                                tgt_lang="ru_RU")
+model.train(tokenized_datasets[2]["train"],
+            tokenized_datasets[2]["validation"],
+            "{}/en_XX_tuned_es_XX_and_ru_RU".format(output_dir),
+            save_model=True)
+for tokenized_dataset, model_language in zip(tokenized_datasets, model_languages):
+    model = MBartSummarizationModel(model_name="{}/en_XX_tuned_es_XX_and_ru_RU".format(output_dir),
+                                    src_lang=model_language,
+                                    tgt_lang=model_language,
+                                    output_dir="{}/en_XX_tuned_es_XX_and_ru_RU_{}".format(output_dir, model_language))
+    model.test_predictions(tokenized_dataset["test"])
